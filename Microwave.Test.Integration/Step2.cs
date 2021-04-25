33d1f1b1104540e12573698a403a4335c0348e7d
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-04-25 18:52:56 +0200

    Added tests to step 2

--- Microwave.Test.Integration/Step2.cs
+++ Microwave.Test.Integration/Step2.cs
@@ -1,10 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
-    class Step2
+    public class Step2
     {
+        private Door door;
+        private UserInterface userInterface;
+        private Button uutPowerButton;
+        private Button uutCancelStartButton;
+        private Button uutTimeButton;
+        private ICookController fakeCookController;
+        private IDisplay fakeDisplay;
+        private ILight fakeLight;
+
+        [SetUp]
+        public void Setup()
+        {
+            door = new Door(); 
+            uutPowerButton = new Button();
+            uutCancelStartButton = new Button();
+            uutTimeButton = new Button();
+            fakeCookController = Substitute.For<ICookController>();
+            fakeDisplay = Substitute.For<IDisplay>();
+            fakeLight = Substitute.For<ILight>();
+            userInterface = new UserInterface(uutPowerButton,
+                uutTimeButton,
+                uutCancelStartButton,
+                door,
+                fakeDisplay,
+                fakeLight,
+                fakeCookController);
+        }
+
+        [Test]
+        public void Press_PowerButtonPress_UIReceivedOnPowerPressedEvent()
+        {
+            //Arrange
+            //Act
+            uutPowerButton.Press();
+            //Assert
+            fakeDisplay.Received(1).ShowPower(Arg.Any<int>());
+        }
+
+        [Test]
+        public void Press_TimeButtonPress_UIReceivedOnTimerPressedEvent()
+        {
+            //Arrange
+            uutPowerButton.Press();
+            //Act
+            uutTimeButton.Press();
+            //Assert
+            fakeDisplay.Received(1).ShowTime(Arg.Any<int>(), Arg.Any<int>());
+        }
     }
 }
