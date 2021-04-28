e25cfd4c3054e46ff49a7b05f42975fce63f07f2
RStavnager <33657263+RStavnager@users.noreply.github.com>
2021-04-28 10:53:46 +0200

    fixed error in timer.

--- Microwave.Classes/Boundary/Timer.cs
+++ Microwave.Classes/Boundary/Timer.cs
@@ -43,7 +43,7 @@ namespace Microwave.Classes.Boundary
         {
             // One tick has passed
             // Do what I should
-            TimeRemaining -= 1000;
+            TimeRemaining -= 1; //ERROR WAS HERE!!! value was set to 1000
             TimerTick?.Invoke(this, EventArgs.Empty);
 
             if (TimeRemaining <= 0)
